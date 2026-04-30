36f07263d27e8eff874cbca747b6f5c3975adbb6
bicrupeshkumar2025-collab <25016818@hope.ac.uk>
2026-04-30 11:50:08 +0100

    class

---
+++ prog.py
@@ -0,0 +1,44 @@
+import pandas as pd
+
+# df = pd.read_csv('book_recommendation_dataset.csv')
+
+# df.info()
+
+# This class will handle taking the username and information
+class user():
+    def __init__(self, username, email, contact):
+        self.username = username
+        self.email = email
+        self.contact = contact    
+
+class preferences():
+    
+    
+    #  This class will handle collecting of preferences for the user
+
+    pass
+
+
+
+
+
+class recommender():
+
+    # This class will make recommendation based on the users preferences
+    
+    pass
+
+
+
+
+def main():
+    
+
+    pass
+
+
+
+
+
+if __name__ == "__main__":
+    main()
