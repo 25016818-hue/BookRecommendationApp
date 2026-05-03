08d6d109c13172a08980e80bb7819b89113cee0f
bicrupeshkumar2025-collab <25016818@hope.ac.uk>
2026-05-03 11:02:01 +0100

    preferences

--- prog.py
+++ prog.py
@@ -5,33 +5,52 @@ df = pd.read_csv('book_recommendation_dataset.csv')
 df.head()
 print(df['author'].unique)
 print(" ")
-print(df['category'].unique)
+aaaa = df['category'].unique
+
+
+aaaa.value_count()
+
+
+
+
+
 
 # df.info()
 
 # This class will handle taking the username and information
-class User():
-    def __init__(self, username, email, contact):
-        self.username = username
-        self.email = email
-        self.contact = contact    
+class User:
+    def __init__(self):
+        self.username = username = input("Please enter your Name: " )
+        self.email = email = input("Please enter your E-mail: " )
+        self.contact = contact = input("Please enter your contact: " )
 
-
-
-class Preferences():
-
+        return
     
-    #  This class will handle collecting of preferences for the user
-
-    pass
-
-
+# rrrrr = User()
+# print(rrrrr.email)
+ 
 
 
+#  This class will handle collecting of preferences for the user 
+class Preferences:
 
+    def __init__(self, author, category, rating):
+        self.author = author
+        self.category = category
+        self.rating = input("Please enter your Book Rating between 1 and 5: " )
+    
+
+
+
+
+
+
+# This class will make recommendation based on the users preferences
 class Recommender():
 
-    # This class will make recommendation based on the users preferences
+    def __init__(self, )
+
+    
     
     pass
 
