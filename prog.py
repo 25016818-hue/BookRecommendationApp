4caab77e4dffe398cd26861901c8bbc40c62ed67
bicrupeshkumar2025-collab <25016818@hope.ac.uk>
2026-05-11 16:31:54 +0100

    new

--- prog.py
+++ prog.py
@@ -2,13 +2,13 @@ import pandas as pd
 
 df = pd.read_csv('book_recommendation_dataset.csv')
 
-df.head()
+print(df.head())
 print(df['author'].unique)
 print(" ")
 aaaa = df['category'].unique
 
 
-aaaa.value_count()
+#aaaa.value_count()
 
 
 
@@ -25,22 +25,35 @@ class User:
         self.contact = contact = input("Please enter your contact: " )
 
         return
-    
-# rrrrr = User()
-# print(rrrrr.email)
- 
+""" 
+rrrrr = User()
+print(rrrrr.email)
+"""
 
 
 #  This class will handle collecting of preferences for the user 
 class Preferences:
 
-    def __init__(self, author, category, rating):
-        self.author = author
-        self.category = category
-        self.rating = input("Please enter your Book Rating between 1 and 5: " )
-    
+    def __init__(self):
+        self.author = ""
+        self.category = ""
+        self.rating = ""    
+
+    def collect_preferences(self):
+        self.author = input("Please Enter your prefered author: ")
+        self.category = input("Please Enter your prefered category: ")
+        self.rating = input("Please Enter your prefered rating between 1 and 5: " )
+
+    def display_preferences(self):
+        print("\nUser Preferences")
+        print("----------------- You have selected -----------------")
+        print(f"author: {self.author}")
+        print(f":category {self.category}")
+        print(f":rating {self.rating}")
 
 
+bb = Preferences()
+bb.collect_preferences()
 
 
 
@@ -48,10 +61,9 @@ class Preferences:
 # This class will make recommendation based on the users preferences
 class Recommender():
 
-    def __init__(self, )
+    def __init__(self, ):
+        pass
 
-    
-    
     pass
 
 
